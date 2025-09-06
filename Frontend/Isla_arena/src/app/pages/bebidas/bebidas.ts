8147d881078b2422ba70b9a0fd359921744f4cac
Rubissell <8456@itescam.edu.mx>
2025-09-06 17:16:23 -0600

    20elementos nuevos

---
+++ Frontend/Isla_arena/src/app/pages/bebidas/bebidas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bebidas',
+  imports: [],
+  templateUrl: './bebidas.html',
+  styleUrl: './bebidas.css'
+})
+export class Bebidas {
+
+}
---
+++ Frontend/Isla_arena/src/app/pages/gestor/gestor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestor',
+  imports: [],
+  templateUrl: './gestor.html',
+  styleUrl: './gestor.css'
+})
+export class Gestor {
+
+}
