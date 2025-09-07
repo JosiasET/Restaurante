874a18718ae3baad3faf4d6b80328ffb40e561e5
Rubissell <8456@itescam.edu.mx>
2025-09-06 21:34:23 -0600

    cambios en la parte menu xd

--- Frontend/Isla_arena/src/app/app.routes.ts
+++ Frontend/Isla_arena/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { Layout } from './componet/layout/layout';
 import { Home } from './pages/home/home';
 import { Bebidas } from './pages/bebidas/bebidas';
+import { Alimentos } from './pages/alimentos/alimentos';
 
 export const routes: Routes = [
     {path:'home', component: Home},
-    {path:'bebida',component:Bebidas}
+    {path:'bebida',component:Bebidas},
+    {path:'alimentos', component:Alimentos}
 ];
 
 
--- Frontend/Isla_arena/src/app/pages/home/home.ts
+++ Frontend/Isla_arena/src/app/pages/home/home.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
+  imagenes = [
+    'assets/img/img1.jpg',
+    'assets/img/img2.jpg',
+    'assets/img/img3.jpg',
+    'assets/img/img4.jpg'
+  ];
+  imagenActual = 0;
 
+  ngOnInit() {
+    setInterval(() => {
+      this.imagenActual = (this.imagenActual + 1) % this.imagenes.length;
+    }, 4000); // cada 4 segundos cambia
+  }
 }
