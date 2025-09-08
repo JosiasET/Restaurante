cda1bb539f6a113384caa2f116607dc246990a26
Rubissell <8456@itescam.edu.mx>
2025-09-07 22:43:49 -0600

    cambios culonas

--- Frontend/Isla_arena/src/app/app.routes.ts
+++ Frontend/Isla_arena/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Layout } from './componet/layout/layout';
 import { Home } from './pages/home/home';
 import { Bebidas } from './pages/bebidas/bebidas';
 import { Alimentos } from './pages/alimentos/alimentos';
+import { Administrador } from './componet/administrador/administrador';
 
 export const routes: Routes = [
     {path:'home', component: Home},
     {path:'bebida',component:Bebidas},
-    {path:'alimentos', component:Alimentos}
+    {path:'alimentos', component:Alimentos},
+    {path:'administrador', component:Administrador}
 ];
 
 
